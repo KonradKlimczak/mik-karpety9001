f215ad7e9e91505d180e74a44a5cb7b620592cce
Konrad <konradklimczak93@gmail.com>
2020-06-07 17:15:54 +0200

    mobile friendly nav bar started

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,17 @@
-import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  Drawer,
+  IconButton,
+  List,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import { Menu as MenuIcon } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -13,11 +23,35 @@ const useStyles = makeStyles((theme) => ({
 
 export const NavBar: FC = (props) => {
   const classes = useStyles();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const matches = useMediaQuery('(min-width: 900px)');
+
+  const menu = (
+    <>
+      <Button color="inherit" component={RouterLink} to="/">
+        Strona główna
+      </Button>
+      <Button color="inherit" component={RouterLink} to="/socks">
+        Oferta
+      </Button>
+      <Button color="inherit" component={RouterLink} to="/about">
+        O Firmie
+      </Button>
+      <Button color="inherit" component={RouterLink} to="/kontakt">
+        Kontakt
+      </Button>
+    </>
+  );
 
   return (
     <div>
       <AppBar position="static" color="primary">
         <Toolbar>
+          {!matches && (
+            <IconButton color="inherit" onClick={() => setDrawerOpen(true)}>
+              <MenuIcon />
+            </IconButton>
+          )}
           <Typography
             variant="h6"
             className={classes.title}
@@ -26,20 +60,24 @@ export const NavBar: FC = (props) => {
           >
             Mik Skarpety
           </Typography>
-          <Button color="inherit" component={RouterLink} to="/">
-            Strona główna
-          </Button>
-          <Button color="inherit" component={RouterLink} to="/socks">
-            Oferta
-          </Button>
-          <Button color="inherit" component={RouterLink} to="/about">
-            O Firmie
-          </Button>
-          <Button color="inherit" component={RouterLink} to="/kontakt">
-            Kontakt
-          </Button>
+          {matches && menu}
         </Toolbar>
       </AppBar>
+
+      {!matches && (
+        <Drawer
+          anchor="left"
+          open={drawerOpen}
+          onClose={() => setDrawerOpen(false)}
+        >
+          <List
+            style={{ display: 'flex', flexDirection: 'column' }}
+            onClick={() => setDrawerOpen(false)}
+          >
+            {menu}
+          </List>
+        </Drawer>
+      )}
     </div>
   );
 };
